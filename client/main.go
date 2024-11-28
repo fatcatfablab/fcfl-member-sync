86f606ad5e1de9bf5d7112479da9f0beb7a4cb5b
Miquel Ruiz <self@miquelruiz.net>
2024-11-28 00:05:36 -0500

    Add mTLS

--- client/main.go
+++ client/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"log"
+	"os"
 	"time"
 
 	pb "github.com/miquelruiz/fcfl-member-sync/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
@@ -18,7 +21,28 @@ var (
 func main() {
 	flag.Parse()
 
-	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	cert, err := tls.LoadX509KeyPair("certs/client.crt", "certs/client.key")
+	if err != nil {
+		log.Fatalf("failed to load client cert: %v", err)
+	}
+
+	ca := x509.NewCertPool()
+	caFilePath := "certs/root_ca.crt"
+	caBytes, err := os.ReadFile(caFilePath)
+	if err != nil {
+		log.Fatalf("error reading ca %q: %v", caFilePath, err)
+	}
+	if !ca.AppendCertsFromPEM(caBytes) {
+		log.Fatalf("failed to parse %q", caFilePath)
+	}
+
+	tlsConfig := &tls.Config{
+		ServerName:   "localhost",
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      ca,
+	}
+
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
 	if err != nil {
 		log.Fatalf("couldn't connect: %v", err)
 	}
--- server/main.go
+++ server/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/miquelruiz/fcfl-member-sync/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
@@ -27,13 +31,36 @@ func (s *server) List(_ context.Context, _ *pb.Empty) (*pb.MemberList, error) {
 func main() {
 	log.Println("Oh, hai!")
 	flag.Parse()
+
+	cert, err := tls.LoadX509KeyPair("certs/server.crt", "certs/server.key")
+	if err != nil {
+		log.Fatalf("error loading certs: %v", err)
+	}
+
+	ca := x509.NewCertPool()
+	caFilePath := "certs/root_ca.crt"
+	caBytes, err := os.ReadFile(caFilePath)
+	if err != nil {
+		log.Fatalf("error reading %q: %v", caFilePath, err)
+	}
+	if !ca.AppendCertsFromPEM(caBytes) {
+		log.Fatalf("error adding CA to pool")
+	}
+
+	tlsConfig := &tls.Config{
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    ca,
+	}
+
+	s := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)))
+	pb.RegisterMembershipServer(s, &server{})
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("couldn't listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	pb.RegisterMembershipServer(s, &server{})
 	log.Printf("server listening on %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("error serving: %v", err)
