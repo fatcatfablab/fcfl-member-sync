8a04036fc09fe93d40f61beaa081e0ece99bbc4e
Miquel Ruiz <self@miquelruiz.net>
2025-05-29 23:23:21 -0400

    Remove customers schema

--- schema/webhook/customers.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `customers` (
-    `customer_id` VARCHAR(255),
-    `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`customer_id`),
-    UNIQUE KEY (`email`)
-);
